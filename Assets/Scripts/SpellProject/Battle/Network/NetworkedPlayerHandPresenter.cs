046577ec6da71c3451b9f983f3849b301759018c
たけのこ <takenokopika@gmail.com>
2025-01-06 03:16:31 +0900

    結局FusionのエラーはNetworkArrayの書き方のミスでした。

--- Assets/Scripts/SpellProject/Battle/Network/NetworkedPlayerHandPresenter.cs
+++ Assets/Scripts/SpellProject/Battle/Network/NetworkedPlayerHandPresenter.cs
@@ -4,5 +4,7 @@ namespace SpellProject.Battle.Network
 {
     public class NetworkedPlayerHandPresenter : NetworkBehaviour
     {
+        [Networked, Capacity(4)]  NetworkArray<byte> NetworkedHand { get; }
+        public void SetValue(int index, byte value) => NetworkedHand.Set(index, value);
     }
 }
